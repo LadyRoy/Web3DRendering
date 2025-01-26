cfe003b7f27bb3cccd68ea8f232ebb24e2885818
LadyRoy <add.mapcejib@mail.ru>
2025-01-26 21:35:02 +0700

    Вожусь с сессиями

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import os
 import bpy
 import time
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, jsonify, redirect, session
+from flask_session import Session
 
 current_dir = os.getcwd()
 path_to_jpeg_folder = os.path.join(current_dir, "../uploads")
 hdri_image_path = os.path.join(current_dir, "../assets", "studio.hdr")
 uploads_folder = os.path.join(current_dir, "../uploads")
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False #время жизни сессии, при закрытии браузера закроется.
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route('/')
 def show_main_page():
@@ -17,15 +22,15 @@ def show_main_page():
 def handle_file_upload():
     if 'file' not in request.files:
         return "Файл не выбран", 400
-    
+
     uploaded_file = request.files['file']
-    
+
     if uploaded_file.filename == '':
         return "Файл не выбран", 400
-    
+
     upload_path = os.path.join(uploads_folder, uploaded_file.filename)
     uploaded_file.save(upload_path)
-    
+
     try:
 
     # Импортируем GLB файл
@@ -34,25 +39,25 @@ def handle_file_upload():
         if "Cube" in bpy.data.objects:
             object_to_delete = bpy.data.objects["Cube"]
             bpy.data.objects.remove(object_to_delete, do_unlink=True)
-        
+
         # Настройка HDRI
         world = bpy.context.scene.world
         if world is None:
             world = bpy.data.worlds.new("World")
             bpy.context.scene.world = world
-            
+
         world.use_nodes = True
-        
+
         # Добавляем узлы
         env_texture_node = world.node_tree.nodes.new(type='ShaderNodeTexEnvironment')
         env_texture_node.image = bpy.data.images.load(hdri_image_path)
 
         # Текстурные координаты
         tex_coord_node = world.node_tree.nodes.new(type='ShaderNodeTexCoord')
-        
+
         # Отображение
         mapping_node = world.node_tree.nodes.new(type='ShaderNodeMapping')
-        
+
         # Фоновый узел
         bg_node = world.node_tree.nodes.get("Background")
         if bg_node is None:
@@ -62,8 +67,6 @@ def handle_file_upload():
         links.new(tex_coord_node.outputs['Generated'], mapping_node.inputs['Vector'])
         links.new(mapping_node.outputs['Vector'], env_texture_node.inputs['Vector'])
         links.new(env_texture_node.outputs['Color'], bg_node.inputs['Color'])
-        # Установка количества семплов 
-        bpy.context.scene.cycles.samples = 128
 
         # Настройка выхода
         output_filename = os.path.splitext(uploaded_file.filename)[0] + '.jpeg'
@@ -71,6 +74,8 @@ def handle_file_upload():
 
         bpy.context.scene.render.filepath = output_filepath
         bpy.context.scene.render.image_settings.file_format = 'JPEG'
+        # Установка количества семплов
+        #bpy.context.scene.cycles.samples = 128
         start_time = time.time()
         # Рендер и сохранение
 
@@ -87,9 +92,25 @@ def handle_file_upload():
     
     return render_template('index.html', download_link=output_filename, render_time=render_time)
 
-def render_stats(scene):
+# Надо подумать условие, при котором будет проверка новых данных и обновлять ее
+def long_polling():
+    start_time = time.time()
+    timeout = 20
+    while time.time() - start_time < timeout:
 
-    print(f"В процессе..")
+        #if :
+            time.sleep(1)
+
+    return jsonify({"message": "Timeout"}), 204
+
+#Запись в сессию статы и времени
+def render_stats(scene):
+    session['render_stats'] = []
+    session['time'] = time.time()
+
+
+#def render_stats(scene):
+ #   print(f"В процессе..")
 def save(scene):
     print("Сохранение изображения..")
 def end(scene):
